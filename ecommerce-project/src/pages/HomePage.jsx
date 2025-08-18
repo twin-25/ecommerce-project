d89bf483f681c1720ccdec5c725da5b0083caff3
twin-25 <tjssg678@gmail.com>
2025-08-18 19:26:42 -0400

    fetch data ffrom the backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -5,6 +5,14 @@ import { products } from "../../starting-code/data/products";
 
 
 export function HomePage() {
+
+  fetch('http://localhost:3000/api/products')
+  .then((response) =>{
+    return response.json()
+    }).then((data) =>{
+      console.log(data);
+
+  })
   return (
     <>
       <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
