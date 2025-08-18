840b33e9d5243b4b3b56b057b5bc5775242bdea4
twin-25 <tjssg678@gmail.com>
2025-08-18 19:30:52 -0400

    use axios to get data

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -2,16 +2,14 @@
 import { Header } from "../components/Header";
 import './HomePage.css';
 import { products } from "../../starting-code/data/products";
+import axios from 'axios';
 
 
 export function HomePage() {
 
-  fetch('http://localhost:3000/api/products')
+  axios.get('http://localhost:3000/api/products')
   .then((response) =>{
-    return response.json()
-    }).then((data) =>{
-      console.log(data);
-
+    console.log(response.data)
   })
   return (
     <>
