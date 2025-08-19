338acfb79476631b7efee09c266a4d7189ea53f6
twin-25 <tjssg678@gmail.com>
2025-08-19 17:51:04 -0400

    create delete cart item feature

--- ecommerce-project/src/pages/checkout/CartItemDetails.jsx
+++ ecommerce-project/src/pages/checkout/CartItemDetails.jsx
@@ -1,6 +1,12 @@
 import { formatMoney } from "../../utils/money"
 import { DeliveryOptions } from "./DeliveryOptions"
+import axios from "axios"
 export function CartItemDetails({cartItem, deliveryOptions, loadCart}) {
+
+  const deleteCartItem = async () =>{
+    await axios.delete(`/api/cart-items/${cartItem.productId}`)
+    await loadCart();
+  }
   return (<>
     
                   <img className="product-image"
@@ -20,7 +26,8 @@ export function CartItemDetails({cartItem, deliveryOptions, loadCart}) {
                       <span className="update-quantity-link link-primary">
                         Update
                       </span>
-                      <span className="delete-quantity-link link-primary">
+                      <span className="delete-quantity-link link-primary"
+                      onClick={deleteCartItem}>
                         Delete
                       </span>
                     </div>
